1b1053df1596dd9994ffaad94d16934986fe58f6
William Henney <will@henney.org>
2016-12-14 08:55:47 +0000

    Earlier changes but still using Alba knot list

--- test-sh-args.sh
+++ test-sh-args.sh
@@ -1,4 +1,7 @@
 files=$(find $PWD/Calibrated/BGsub -name "$1-vhel.fits")
-for f in $files; do
-    ls -l $f
+regdir=Alba-Regions-2016-10/blue_knots_final-SLITS
+for path in $files; do
+    f=${path##/*/} # just the filename
+    regfile=$regdir/$(basename $f .fits).reg
+    ls -l $regfile
 done
